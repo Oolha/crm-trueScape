1dd729b9fbba700bd58533e4222d77a356f144c8
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-01-30 02:03:10 +0100

    add using react-query

--- crm/src/app/(admin)/companies/@toolbar/[id]/page.tsx
+++ crm/src/app/(admin)/companies/@toolbar/[id]/page.tsx
@@ -1,4 +1,14 @@
-export interface PageProps {}
-export default function Page({}: PageProps) {
-  return null;
+import Toolbar from '@/app/components/toolbar';
+import SearchInput from '@/app/components/search-input';
+import AddPromotionButton from '@/app/components/add-promotion-button';
+
+export interface PageProps {
+  params: { id: string };
+}
+export default function Page({ params }: PageProps) {
+  return (
+    <Toolbar action={<AddPromotionButton companyId={params.id} />}>
+      <SearchInput />
+    </Toolbar>
+  );
 }
--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,12 +1,9 @@
-import AddCompanyButton from './components/add-company-button';
-
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold text-center text-gray-800">
         Home Page
       </h1>
-      <AddCompanyButton />
     </main>
   );
 }
