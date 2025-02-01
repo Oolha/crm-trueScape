309203cb8e4a9967943c2fb835a388781038c83f
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-02-01 01:54:06 +0100

    remove lootie library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
 'use client';
 import Link from 'next/link';
-import Lottie from 'lottie-react';
-// import animationData from "../public/animation.json"; // Завантаж свою анімацію
 
 export default function Home() {
   return (
     <div className="relative flex flex-col items-center justify-center h-screen text-center bg-gradient animate-gradient text-white">
-      {/* Анімація Lottie */}
-      {/* <Lottie animationData={animationData} className="w-80 h-80" /> */}
-
-      {/* Заголовок */}
       <h1 className="text-5xl font-bold drop-shadow-lg">
         Welcome to CRM for Vendors
       </h1>
       <p className="text-lg mt-4 opacity-90">
         Manage vendors, promotions, and sales effortlessly.
       </p>
-
-      {/* Кнопки */}
       <div className="mt-6 flex space-x-6">
         <Link
           href="/companies"
