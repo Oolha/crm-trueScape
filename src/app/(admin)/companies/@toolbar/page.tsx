e73a3a9de31ce2620963f1541f5f0bcaa620960f
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-02-02 16:57:09 +0100

    add suspense to company page

--- src/app/(admin)/companies/@toolbar/page.tsx
+++ src/app/(admin)/companies/@toolbar/page.tsx
@@ -1,11 +1,14 @@
 import Toolbar from '@/app/components/toolbar';
 import SearchInput from '@/app/components/search-input';
 import AddCompanyButton from '@/app/components/add-company-button';
+import { Suspense } from 'react';
 
 export default function Page({}) {
   return (
     <Toolbar action={<AddCompanyButton />}>
-      <SearchInput />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SearchInput />
+      </Suspense>
     </Toolbar>
   );
 }
--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -2,7 +2,7 @@ import CompanyTable from '@/app/components/company-table';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { getCompanies } from '@/lib/api';
 import getQueryClient from '@/lib/utils/getQueryClient';
-
+import { Suspense } from 'react';
 
 export default async function Page({}) {
   const queryClient = getQueryClient();
@@ -14,7 +14,18 @@ export default async function Page({}) {
   const dehydratedState = dehydrate(queryClient);
   return (
     <HydrationBoundary state={dehydratedState}>
-      <CompanyTable />
+      <Suspense
+        fallback={
+          <div className="py-8 px-10 bg-gray-100">
+            <div className="flex items-center justify-center">
+              <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900" />
+              <span className="ml-2">Loading companies...</span>
+            </div>
+          </div>
+        }
+      >
+        <CompanyTable />
+      </Suspense>
     </HydrationBoundary>
   );
 }
