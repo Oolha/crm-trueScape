dffa67aa67e068113dc26f0a372e6f276f251bc1
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-02-02 19:14:50 +0100

    resolve params type error

--- src/app/(admin)/companies/[id]/new-promotion/page.tsx
+++ src/app/(admin)/companies/[id]/new-promotion/page.tsx
@@ -1,17 +1,21 @@
 import type { Metadata } from 'next';
 import PromotionForm from '@/app/components/promotion-form';
 
-type PageParams = { id: string };
-
-export interface GenerateMetadata {
-  params: PageParams;
-}
+export type GenerateMetadata = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
 export const metadata: Metadata = {
   title: 'New Promotion - CRM TrueScape',
 };
 
-export default function Page({ params }: { params: PageParams }) {
+export default function Page({
+  params,
+}: {
+  params: { id: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
   return (
     <div className="py-6 px-10">
       <PromotionForm companyId={params.id} />
